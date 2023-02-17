0414dd8835e0b9659a555cf5121d82cd8beeb214
Ritesh Raj <76565131+Riteshrajcloud@users.noreply.github.com>
2023-02-17 15:15:52 +0530

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int a,b;
+ cin>>a>>b;
+ cout<<"sum="<<a+b<<endl;
+ cout<<"Difference="<<a-b<<endl;
+ cout<<"Product="<<a*b<<endl;
+ cout<<"Division="<<float(a/b)<<endl;
+}
